71e21eae06b012b11310bd2f6455402f7b7a0963
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-03-19 12:18:16 +0800

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+import { ENV } from './config/env.js'
+
+const app = express();
+
+app.listen( ENV.PORT, () => {
+    console.log(`Server running on PORT: ${ENV.PORT}`)
+})
